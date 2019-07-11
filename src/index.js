0a5bac0e84d89b63967a60c133d924521854a674
zacanger <zac@zacanger.com>
2019-07-10 19:11:10 -0600

    Remove Flow support
    It's time to let go of the type system by one big evil corp.
    Maybe I'll bring in the type system by the other big evil
    corp, eventually.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-// @flow
+/* eslint-disable react/prop-types */
 
 import * as React from 'react'
 import { withRouter, Redirect } from 'react-router'
 import normalize from 'path-normalize'
 
-type NormalizeProps = {
-  location: { pathname: string },
-  children: React.Node
-}
-
-const handleTraversal = (path: string): string =>
+const handleTraversal = (path) =>
   path
     .split('/')
-    .filter((s: string): bool => !/^\.{2,}$/.test(s))
+    .filter((s) => !/^\.{2,}$/.test(s))
     .join('/')
 
 const Normalize = ({
   location: { pathname = '' },
   children
-}: NormalizeProps): React.Node => {
+}) => {
   const normalized = normalize(handleTraversal(pathname))
   return normalized === pathname
     ? children
     : <Redirect to={normalized} />
 }
 
-// $FlowFixMe
 export default withRouter(Normalize)
